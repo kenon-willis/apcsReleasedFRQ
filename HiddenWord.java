226b580b3ab9db5668d8b91f17ad3175e9b98965
Kenon Willis <kwillis@lvjusd.k12.ca.us>
2019-10-02 13:22:56 -0700

    pushed hidden word

---
+++ HiddenWord.java
@@ -0,0 +1,21 @@
+public class HiddenWord
+   {
+   private String word;
+   
+   public HiddenWord( String initialWord )
+      {
+      word = new String( initialWord );    
+      } // end one-arg constructor
+       
+      
+   public String getHint( String guess )  
+      {
+      
+      return null;    
+      } // end method getHint
+      
+   public String toString()
+      {
+      return word;   
+      } // end method toString
+   } // end class HiddenWord
